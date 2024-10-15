417c4444282083d919e80438268819530481e186
lakazatong <Bo_wood@outlook.fr>
2024-10-16 00:26:14 +0200

    Update Globals.cs

--- osu.Framework/BellaFiora/Globals.cs
+++ osu.Framework/BellaFiora/Globals.cs
@@ -8,8 +8,8 @@ namespace osu.Framework.BellaFiora
         public static readonly bool USE_CUSTOM_GAME_HOST = true;
         public static readonly bool NO_INPUT_HANDLERS = false;
         public static readonly double DRAW_THREADS_HZ = 60f;
-        public static readonly double UPDATE_THREADS_HZ = 30f; // no impact on Auto gameplay (takes longer to start a map tho)
-        public static readonly double INPUT_THREADS_HZ = 10f; // no impact on Auto gameplay
+        public static readonly double UPDATE_THREADS_HZ = 60f; // no impact on Auto gameplay (takes longer to start a map tho)
+        public static readonly double INPUT_THREADS_HZ = 60f; // no impact on Auto gameplay
         public static readonly double AUDIO_THREADS_HZ = 60f;
         public static readonly bool MUTE_MUSIC = false;
         public static readonly bool MUTE_EFFECTS = false;
